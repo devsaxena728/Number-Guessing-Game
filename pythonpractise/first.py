9d54662fe3a5b6f940c26c8111c2a68c7205727f
devsaxena728 <74059105+devsaxena728@users.noreply.github.com>
2021-04-23 21:16:12 +0530

    Update first.py

--- pythonpractise/first.py
+++ pythonpractise/first.py
@@ -1,12 +1,28 @@
-def swapfile():
-    file1=input('enter files name')
-    file2=input('enter files name')
-    with open(file1,'r') as a:
-        data_a=a.read()
-    with open(file2,'r') as b:
-        data_b=b.read()
-    with open(file1,'w') as a:
-        a.write(data_b)
-    with open(file2,'w') as b:
-        b.write(data_a)
-swapfile()
+import os
+import time
+import shutil
+
+path = input("Enter your path: ")
+
+days = 30
+seconds = time.time() - (days * 24 * 60 * 60)
+
+if os.path.exists(path):
+    for root, dirs, files in os.walk(path, topdown=True):
+        for name in files:
+            path = os.path.join(root, name)
+            ctime = os.stat(path).st_ctime
+
+            if seconds >= ctime:
+                os.remove(path)
+                print("\n Deleted the path " + path + " successfully")
+
+        for name in dirs:
+            path = os.path.join(root, name)
+            ctime = os.stat(path).st_ctime
+
+            if seconds >= ctime:
+                shutil.rmtree(path)
+                print("\n Deleted the path " + path + " successfully")
+else:
+    print("\n Path not found")
